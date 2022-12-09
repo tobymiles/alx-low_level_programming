62a65a116107223a328b72eaa07ae99bffe076c6
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2022-12-09 04:54:17 -0800

    Task 5 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
-/*
- *
+/**
  * main - main block
  * 
  * Return : Always 0 
- *
  */
 
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 
