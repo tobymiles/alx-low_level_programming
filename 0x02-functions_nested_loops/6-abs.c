660e27aea3b52896ce1deb225ebd264734ae0ca2
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2022-12-13 12:36:17 -0800

    Task 6 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * Return: The absolute calue of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
