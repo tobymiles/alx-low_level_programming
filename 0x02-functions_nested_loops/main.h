cacb860c889ba9397106cfa03664772f52b15875
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2022-12-13 13:16:47 -0800

    Task 9 9-times_table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 void jack_bauer(void);
 int _putchar(char c);
 #endif
