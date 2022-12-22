bb16b4b14ec9033a9cbdad667d39649867d24fe0
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2022-12-22 13:55:05 -0800

    Task 2 2-strncpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,27 +1,24 @@
-#ifndef MAIN_H
+#ifndef MAIN_H 
 #define MAIN_H
 
 /**
  * File: main.h
- * Author: Tobechukwu
- * Description: Header file containing prototypes for all functions
- * used in the 0x04-pointers_arrays_strings directory.
+ * Auth: Ekwealor Tobechukwu
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings directory.
  */
 
-#include <stdlib.h>
-
+#include <stddef.h>
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
