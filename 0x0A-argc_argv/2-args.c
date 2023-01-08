29be039b34917459ea26355f5c73287ef8c05d41
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2023-01-08 04:08:06 -0800

    Task 2 of argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,12 +2,12 @@
 #include "main.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
- *  * main - Print the name of the program                                                                                              
- *   * @argc: Count arguments                                                                                                            
- *    * @argv: Arguments                                                                                                                  
- *     *                                                                                                                                   
- *      * Return: Always 0 (Success)                                                                                                        
- *       */                                                                                                                                  
+ * main - Print the name of the program                                                                                              
+ * @argc: Count arguments                                                                                                            
+ * @argv: Arguments                                                                                                                  
+ *                                                                                                                                   
+ * Return: Always 0 (Success)                                                                                                        
+ */                                                                                                                                  
                                                                                                                                      
 int main(int argc, char *argv[])                                                                                                     
 {                                                                                                                                    
