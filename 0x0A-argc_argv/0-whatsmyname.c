c3cf6aad18a6e3100ccb0f0e683292e15cfd019a
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2023-01-08 03:55:45 -0800

    Task 0 of argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,17 @@
 #include "main.h"                                                                                                                    
                                                                                                                                      
 /**                                                                                                                                  
- *  * main - Print the name of the program                                                                                              
- *   * @argc: Count arguments                                                                                                            
- *    * @argv: Arguments                                                                                                                  
- *     *                                                                                                                                   
- *      * Return: Always 0 (Success)                                                                                                        
- *       */                                                                                                                                  
+ * main - print the name of the program                                                                                              
+ * @argc: Count arguments                                                                                                            
+ * @argv: Arguments                                                                                                                  
+ *                                                                                                                                    
+ * Return: Always 0 (Success)                                                                                                        
+ */                                                                                                                                  
                                                                                                                                      
 int main(int argc, char *argv[])                                                                                                     
 {                                                                                                                                    
-	(void) argc;                                                                                                                         
-	printf("%s\n", argv[0]);                                                                                                             
+	(void) argv; /*Ignore argv*/                                                                                                         
+	printf("%i\n", argc - 1);                                                                                                            
 	                                                                                                                                     
 	return (0);                                                                                                                          
 }
