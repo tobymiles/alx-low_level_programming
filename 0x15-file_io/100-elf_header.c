759eb1000392f46220bd77ba97d7599e4f47589e
Ekwealor Tobechukwu <ekwealortobechukwu22@gmail.com>
2023-02-07 02:31:43 -0800

    Task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -30,14 +30,14 @@ void check_elf(unsigned char *e_ident)
 
 	for (index = 0; index < 4; index++)
 	{
-	if (e_ident[index] != 127 
-		e_ident[index] != 'E' &&
-		e_ident[index] != 'L' &&
-		e_ident[index] != 'F')
-	{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
-			exit(98);
-	}
+	if (e_ident[index] != 127 &&
+					    e_ident[index] != 'E' &&
+					    		    e_ident[index] != 'L' &&
+							    		    e_ident[index] != 'F')
+				{
+								dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+											exit(98);
+													}
 	}
 }
 
